0722797f259087a69bcb9bbe49c05e404bbfc9d7
victor-timoshin <victor-timoshin@hotmail.com>
2015-09-12 12:12:44 +0300

    Initial commit

---
+++ HttpWebRequest_Helpers/JsonRequestHelper.cs
@@ -0,0 +1,161 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpWebRequestHelpers
+{
+	public class JsonRequestHelper
+	{
+		public static T GetObjectRest<T>(string server, string relativePath, IDictionary<string, string> headers = null)
+		{
+			Uri serverUri = new Uri(server);
+			Uri relativeUri = new Uri(relativePath, UriKind.Relative);
+
+			HttpWebRequest request = WebRequest.Create(new Uri(serverUri, relativeUri)) as HttpWebRequest;
+			request.ContentType = "application/json; charset=utf-8";
+			request.Method = "GET";
+
+			if (headers != null)
+			{
+				foreach (KeyValuePair<string, string> item in headers)
+					request.Headers.Add(item.Key, Convert.ToString(item.Value));
+			}
+
+			try
+			{
+				HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+				if (response.StatusCode != HttpStatusCode.OK)
+				{
+				}
+
+				using (var responseStreamReader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8")))
+				{
+					using (var responseJsonReader = new JsonTextReader(responseStreamReader))
+					{
+						var serializer = new JsonSerializer();
+						return serializer.Deserialize<T>(responseJsonReader);
+					}
+				}
+			}
+			catch (WebException ex)
+			{
+				if (ex.Response != null)
+				{
+					HttpWebResponse errorResponse = ex.Response as HttpWebResponse;
+					using (var responseStreamReader = new StreamReader(errorResponse.GetResponseStream()))
+					{
+						using (var responseJsonReader = new JsonTextReader(responseStreamReader))
+						{
+							var serializer = new JsonSerializer();
+							return serializer.Deserialize<T>(responseJsonReader);
+						}
+					}
+				}
+
+				return default(T);
+			}
+			finally
+			{
+				request.Abort();
+			}
+		}
+
+		public static async Task<T> GetObjectRestAsync<T>(string server, string relativePath)
+		{
+			Uri serverUri = new Uri(server);
+			Uri relativeUri = new Uri(relativePath, UriKind.Relative);
+
+			HttpWebRequest request = WebRequest.Create(new Uri(serverUri, relativeUri)) as HttpWebRequest;
+			request.ContentType = "application/json; charset=utf-8";
+			request.Method = "GET";
+
+			try
+			{
+				HttpWebResponse response = await Task<WebResponse>.Factory.FromAsync(request.BeginGetResponse(null, null), request.EndGetResponse) as HttpWebResponse;
+				if (response.StatusCode != HttpStatusCode.OK)
+				{
+				}
+
+				using (var responseStreamReader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8")))
+				{
+					using (var responseJsonReader = new JsonTextReader(responseStreamReader))
+					{
+						var serializer = new JsonSerializer();
+						return serializer.Deserialize<T>(responseJsonReader);
+					}
+				}
+			}
+			catch (WebException ex)
+			{
+				if (ex.Response != null)
+				{
+					HttpWebResponse errorResponse = ex.Response as HttpWebResponse;
+					using (var responseStreamReader = new StreamReader(errorResponse.GetResponseStream()))
+					{
+						using (var responseJsonReader = new JsonTextReader(responseStreamReader))
+						{
+							var serializer = new JsonSerializer();
+							return serializer.Deserialize<T>(responseJsonReader);
+						}
+					}
+
+				}
+
+				return default(T);
+			}
+			finally
+			{
+				request.Abort();
+			}
+		}
+
+		public static string PostObjectRest<T>(string server, string relativePath, T data, IDictionary<string, string> headers = null)
+		{
+			Uri serverUri = new Uri(server);
+			Uri relativeUri = new Uri(relativePath, UriKind.Relative);
+
+			var request = WebRequest.Create(new Uri(serverUri, relativeUri)) as HttpWebRequest;
+			request.ContentType = "application/json";
+			request.Method = "POST";
+
+			if (headers != null)
+			{
+				foreach (KeyValuePair<string, string> item in headers)
+					request.Headers.Add(item.Key, Convert.ToString(item.Value));
+			}
+
+			string postData = JsonConvert.SerializeObject(data).ToString();
+			byte[] bytes = new UTF8Encoding().GetBytes(postData);
+
+			request.ContentLength = bytes.Length;
+
+			string message = string.Empty;
+			using (var streamWriter = request.GetRequestStream())
+			{
+				try
+				{
+					streamWriter.Write(bytes, 0, bytes.Length);
+					streamWriter.Flush();
+					streamWriter.Close();
+
+					var response = request.GetResponse() as HttpWebResponse;
+					using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
+					{
+						message = streamReader.ReadToEnd();
+						streamReader.Close();
+					}
+				}
+				catch (Exception ex)
+				{
+					message = "Exception thrown when contacting service: " + ex.ToString();
+				}
+
+				return message;
+			}
+		}
+	}
+}
---
+++ HttpWebRequest_Helpers/XmlRequestHelper.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace HttpWebRequestHelpers
+{
+	public class XmlRequestHelper
+	{
+		public static XDocument GetObjectXML(string server, string relativePath, string token)
+		{
+			Uri serverUri = new Uri(server);
+			Uri relativeUri = new Uri(relativePath, UriKind.Relative);
+
+			HttpWebRequest request = WebRequest.Create(new Uri(serverUri, relativeUri)) as HttpWebRequest;
+			request.Headers.Add("X-Access-Token", token);
+			request.ContentType = "application/xml; charset=utf-8";
+			request.Method = "GET";
+			request.Proxy = null;
+
+			try
+			{
+				HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+				if (response.StatusCode != HttpStatusCode.OK)
+				{
+				}
+
+				using (Stream responseStream = response.GetResponseStream())
+				{
+					return XDocument.Parse(new StreamReader(responseStream).ReadToEnd());
+				}
+			}
+			catch (Exception ex)
+			{
+				var exception = ex.ToString();
+				return default(XDocument);
+			}
+			finally
+			{
+				request.Abort();
+			}
+		}
+	}
+}
